58bb67a12f5a6a77e380a41c46c0a79e6eecd3ef
Valeryia Himzheuskaya <valimisael@yahoo.com>
2019-02-23 20:23:19 +0300

    Advanced Zeros Preliminary Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var baseNumber = base,
+    arrayOfPrimeFactors = [];
+  
+  for (var i = 2; i <= baseNumber; i++) {
+    while (baseNumber % i == 0) {
+      arrayOfPrimeFactors.push(i);
+      baseNumber /= i;
+    }
+  }
+  
+  var largestPrimeFactor = Math.max(...arrayOfPrimeFactors),
+		zeroCount = 0,
+		remainder = number;
+      
+  while (remainder>largestPrimeFactor) {
+    zeroCount += Math.floor(remainder/largestPrimeFactor);
+    remainder = Math.floor(remainder/largestPrimeFactor); 
+  }
+  
+  return zeroCount;
 }
