bb819e41369ff034c2f18c39437d522bd6bdd811
Valeryia Himzheuskaya <valimisael@yahoo.com>
2019-02-24 12:02:21 +0300

    The usage of prime factors degrees is added

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 module.exports = function getZerosCount(number, base) {
+  var baseSystemsQuantity = 256,
+    arrayOfPrimeNumbers = [];
+
+  nextPrime:
+  for (var i = 2; i <= baseSystemsQuantity; i++) {
+    for (var j = 2; j < Math.sqrt(i); j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+    arrayOfPrimeNumbers.push(i);
+  }
+
   var baseNumber = base,
-    arrayOfPrimeFactors = [];
+    arrayOfDegrees = [],
+    largestPrimeFactor = 0;
+    
+  for (var i=0; i < arrayOfPrimeNumbers[arrayOfPrimeNumbers.length-1]; i++) {    
+    arrayOfDegrees[i] = 0;
+  }  
   
-  for (var i = 2; i <= baseNumber; i++) {
-    while (baseNumber % i == 0) {
-      arrayOfPrimeFactors.push(i);
-      baseNumber /= i;
+  for (var i = 0; i <= baseNumber; i++) {
+    while (baseNumber % arrayOfPrimeNumbers[i] == 0) {
+      arrayOfDegrees[arrayOfPrimeNumbers[i]] += 1;
+      largestPrimeFactor = arrayOfPrimeNumbers[i];
+      baseNumber /= arrayOfPrimeNumbers[i];
     }
   }
   
-  var largestPrimeFactor = Math.max(...arrayOfPrimeFactors),
-		zeroCount = 0,
+  var zeroCount = 0,
 		remainder = number;
       
   while (remainder>largestPrimeFactor) {
+    if (remainder % largestPrimeFactor == 0) {
+      zeroCount += remainder/largestPrimeFactor;
+      remainder = remainder/largestPrimeFactor; 
+    }
     zeroCount += Math.floor(remainder/largestPrimeFactor);
     remainder = Math.floor(remainder/largestPrimeFactor); 
   }
   
-  return zeroCount;
+  return Math.floor(zeroCount/arrayOfDegrees[largestPrimeFactor]);
 }
