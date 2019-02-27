ea69b8ce6801238b9b22370c01c7958b28ff9dc4
Valeryia Himzheuskaya <valimisael@yahoo.com>
2019-02-27 17:18:46 +0300

    fix: fix the algorithm to work with all cases

--- src/index.js
+++ src/index.js
@@ -4,39 +4,51 @@ module.exports = function getZerosCount(number, base) {
 
   nextPrime:
   for (var i = 2; i <= baseSystemsQuantity; i++) {
-    for (var j = 2; j < Math.sqrt(i); j++) {
+    for (var j = 2; j <= Math.sqrt(i); j++) {
       if (i % j == 0) continue nextPrime;
     }
     arrayOfPrimeNumbers.push(i);
   }
 
-  var baseNumber = base,
-    arrayOfDegrees = [],
-    largestPrimeFactor = 0;
+  var arrayOfDegrees = [],
+      arrayOfPrimeFactors = [];
     
   for (var i=0; i < arrayOfPrimeNumbers[arrayOfPrimeNumbers.length-1]; i++) {    
     arrayOfDegrees[i] = 0;
   }  
+
+  var baseNumber = base;
   
-  for (var i = 0; i <= baseNumber; i++) {
+  for (var i = 0; i < baseNumber; i++) {
     while (baseNumber % arrayOfPrimeNumbers[i] == 0) {
+      if (arrayOfPrimeFactors.indexOf(arrayOfPrimeNumbers[i]) == -1) {
+        arrayOfPrimeFactors.push(arrayOfPrimeNumbers[i]);  
+      }    
       arrayOfDegrees[arrayOfPrimeNumbers[i]] += 1;
-      largestPrimeFactor = arrayOfPrimeNumbers[i];
-      baseNumber /= arrayOfPrimeNumbers[i];
+      baseNumber = Math.floor(baseNumber/arrayOfPrimeNumbers[i]);
     }
   }
   
-  var zeroCount = 0,
-		remainder = number;
-      
-  while (remainder>largestPrimeFactor) {
-    if (remainder % largestPrimeFactor == 0) {
-      zeroCount += remainder/largestPrimeFactor;
-      remainder = remainder/largestPrimeFactor; 
+  var arrayOfZerosCount = [];
+
+  for (var i = 0; i < arrayOfPrimeFactors.length; i++) {
+    var zerosCount = 0,
+        remainder = number;
+    while(remainder >= arrayOfPrimeFactors[i]) {
+      if (remainder % arrayOfPrimeFactors[i] != 0) {
+        remainder = Math.floor(remainder/arrayOfPrimeFactors[i]);
+        zerosCount += remainder;
+      } else {
+        remainder = remainder/arrayOfPrimeFactors[i];
+        zerosCount += remainder;
+      }
     }
-    zeroCount += Math.floor(remainder/largestPrimeFactor);
-    remainder = Math.floor(remainder/largestPrimeFactor); 
+    if (arrayOfDegrees[arrayOfPrimeFactors[i]] != 0) {
+      arrayOfZerosCount.push(Math.floor(zerosCount/arrayOfDegrees[arrayOfPrimeFactors[i]]));
+    } else {
+      arrayOfZerosCount.push(zerosCount);
+    } 
   }
-  
-  return Math.floor(zeroCount/arrayOfDegrees[largestPrimeFactor]);
+
+  return Math.min(...arrayOfZerosCount);
 }
